ca2645f649af58f8db3b5f679a86417784c80b50
Makar Molochaev <Makara1006@gmail.com>
2025-03-28 17:45:35 +0300

    update storage

--- internal/storage/mysql/mysql.go
+++ internal/storage/mysql/mysql.go
@@ -23,16 +23,82 @@ func New() (*Storage, error) {
 	return &Storage{db: db}, nil
 }
 
-func (s *Storage) SaveAudioFileLink(ctx context.Context, link string) (int64, error) {
-	const op = "storage.mysql.SaveAudioFileLink"
+func (s *Storage) SaveAudioFile(ctx context.Context, link string) (int64, error) {
+	const op = "storage.mysql.SaveAudioFile"
 
-	stmt, err := s.db.Prepare("INSERT INTO logging.audio_file (link, date_au) VALUES (?, ?)")
+	stmt, err := s.db.Prepare("INSERT INTO logging.audio_file (link, date_created) VALUES (?, ?)")
 
 	if err != nil {
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
 
 	res, err := stmt.ExecContext(ctx, link, time.Now().Format("2006-01-02 15:04:05"))
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) SaveTextFile(ctx context.Context, text_full, text_short string) (int64, error) {
+	const op = "storage.mysql.SaveTextFile"
+
+	stmt, err := s.db.Prepare("INSERT INTO logging.text_file (text_full, text_short, date_created) VALUES (?, ?, ?)")
+
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.ExecContext(ctx, text_full, text_short, time.Now().Format("2006-01-02 15:04:05"))
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) SaveValuation(ctx context.Context, usability, processing_speed, processing_quality int, reuse_service bool, comment string) (int64, error) {
+	const op = "storage.mysql.SaveValuation"
+
+	stmt, err := s.db.Prepare("INSERT INTO logging.valuation (usability, processing_speed, processing_quality, reuse_service, comment) VALUES (?, ?, ?, ?, ?)")
+
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.ExecContext(ctx, usability, processing_speed, processing_quality, reuse_service, comment)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) SaveMetrics(ctx context.Context, image_count, guest_count int, av_audio_time, av_process_time float32, satisfy_user_count int) (int64, error) {
+	const op = "storage.mysql.SaveMetrics"
+
+	stmt, err := s.db.Prepare("INSERT INTO logging.metrics (image_count, guest_count, av_audio_time, av_process_time, satisfy_user_count) VALUES (?, ?, ?, ?, ?)")
+
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.ExecContext(ctx, image_count, guest_count, av_audio_time, av_process_time, satisfy_user_count)
 	if err != nil {
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
